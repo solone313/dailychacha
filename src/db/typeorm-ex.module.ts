ef9e710f87d176a255285b8aef19c76028d71aca
hojin <hojin@hojin.local>
2022-09-08 22:12:52 +0900

    fix

--- src/db/typeorm-ex.module.ts
+++ src/db/typeorm-ex.module.ts
@@ -4,6 +4,6 @@ import { SetMetadata } from "@nestjs/common";
 
 export const TYPEORM_EX_CUSTOM_REPOSITORY = "TYPEORM_EX_CUSTOM_REPOSITORY";
 
-export function CustomRepository(entity: Function): ClassDecorator {
+export function CustomRepository(entity): ClassDecorator {
   return SetMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, entity);
 }
